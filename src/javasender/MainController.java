dc437eeafac44f8b8b1e27638d0d983d116cbd76
Maykon Oliveira <maykonoliveira850@gmail.com>
2018-05-10 16:14:57 -0300

    Corrigido o bug

--- src/javasender/MainController.java
+++ src/javasender/MainController.java
@@ -1,45 +1,80 @@
 package javasender;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javasender.config.MailSender;
-import javax.mail.MessagingException;
 import javax.swing.JOptionPane;
+import org.apache.commons.mail.EmailException;
 
 /**
  *
  * @author 20171148060009
  */
 public class MainController implements Initializable {
-    
-    @FXML private Button button;
-    @FXML private TextField DestinatarioTextFiled;
-    @FXML private TextField assuntoTextFiled;
-    @FXML private TextArea corpoMensagem;
-    
+
+    @FXML
+    private TextField DestinatarioTextFiled;
+    @FXML
+    private TextField assuntoTextFiled;
+    @FXML
+    private TextArea corpoMensagem;
+    @FXML
+    private Label nomeAnexoLabel;
+    private File anexo;
+
     MailSender mailSender;
-    
+    FileChooser fileChooser;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         mailSender = new MailSender();
-        System.out.println("b");
-    }    
+        iniciarFileChoose();
+    }
+
+    private void iniciarFileChoose() {
+        fileChooser = new FileChooser();
+        fileChooser.setTitle("Escolher arquivo");
+    }
 
     @FXML
-    private void handleEnviarMensagem(ActionEvent event) throws MessagingException {
-        if (validarCampos()) {
-            mailSender.sendMensagem(assuntoTextFiled.getText(), corpoMensagem.getText(), DestinatarioTextFiled.getText());            
-            JOptionPane.showMessageDialog(null, "Mensagem enviada.");
+    private void handleEnviarMensagem(ActionEvent event) {
+        try {
+            if (validarCampos()) {
+                mailSender.sendMensagem(assuntoTextFiled.getText(), corpoMensagem.getText(), DestinatarioTextFiled.getText(), anexo);
+                limparCampos();
+            }
+
+        } catch (EmailException e) {
+            System.out.println("Erro ao enviar mensagem");
+            e.printStackTrace();
         }
     }
-    
+
     private boolean validarCampos() {
         return (assuntoTextFiled.getText() != null && corpoMensagem.getText() != null) && DestinatarioTextFiled.getText() != null;
+    }
+
+    private void limparCampos() {
+        assuntoTextFiled.clear();
+        DestinatarioTextFiled.clear();
+        corpoMensagem.clear();
+    }
+
+    @FXML
+    private void handleAnexarArquivo(ActionEvent event) {
+        anexo = fileChooser.showOpenDialog(new Stage());
+        
+        if (anexo != null) {
+            nomeAnexoLabel.setText(anexo.getName());
+        }
     }
 }
--- src/javasender/config/MailSender.java
+++ src/javasender/config/MailSender.java
@@ -1,63 +1,50 @@
 package javasender.config;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
 
 /**
  * givanaldoifrn@hotmail.com
  * @author 20171148060009
  */
 public class MailSender {
-    Properties credenciasUsario;
-    Properties configuracaoServidorEmail;
-    UsuarioAuthenticator auth;
-    Session session;
+    Properties credenciasUsario = new Properties();
 
     public MailSender() {
-        credenciasUsario = new Properties();
         loadPreperties();
-        System.out.println("c");
     }    
     private void loadPreperties() {
         try {
             credenciasUsario.load(ClassLoader.getSystemClassLoader().getResourceAsStream("javasender/config/credencias.properties"));
-            // BUG
-            System.out.println("a");
-            configuracaoServidorEmail.load(ClassLoader.getSystemClassLoader().getResourceAsStream("javasender/config/config.properties"));
-            System.out.println("b");
             
         } catch (IOException ex) {
             System.out.println("Erro ao carregar as propriedades");
         }
     }
     
-    public void sendMensagem(String assunto, String mensagem, String destinatario) throws AddressException, MessagingException {
-        if (auth != null) {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(auth.getEmail()));
-            
-            message.addRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
-            
-            message.setSubject(assunto);
-            message.setContent(mensagem, "text/html");
-            Transport.send(message);
-        } else {
-            autenticar();
-            sendMensagem(assunto, mensagem, destinatario);
-        }
-    }
-    
-    private void autenticar() {
-        auth = new UsuarioAuthenticator(credenciasUsario.getProperty("hotmail.email"), credenciasUsario.getProperty("hotmail.password"));
-        // Inicia Debug
-        session = Session.getDefaultInstance(configuracaoServidorEmail, auth);
-        session.setDebug(true);
-    }
+    public void sendMensagem(String assunto, String mensagem, String destinatario, File anexo) throws EmailException {
+        System.out.println("Enviando mensagem...");
+        String username = credenciasUsario.getProperty("hotmail.email");
+        String password = credenciasUsario.getProperty("hotmail.password");
+        
+        
+        Email email = new HtmlEmail();
+        email.setHostName("smtp.live.com");
+        email.setSmtpPort(587);
+        email.setStartTLSRequired(true);
+        email.setAuthenticator(new DefaultAuthenticator(username, password));
+        email.setFrom(credenciasUsario.getProperty("hotmail.email"));
+        email.setSubject(assunto);
+        email.setMsg(mensagem);
+        email.addTo(destinatario);
+        email.setDebug(true);
+        email.send();
+        
+        System.out.println("Mensagem enviada");
+    }    
 }
